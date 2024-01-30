188413065c9438ee9acfe90fd9528e4b2ce58158
Coder.AN <an.hongjun@foxmail.com>
2024-01-31 00:34:56 +0800

    add monitor

--- layouts/head.php
+++ layouts/head.php
@@ -5,4 +5,16 @@
     <title>Coder.AN的四维空间</title>
     <link href="/public/css/style.css" rel="stylesheet">
     <link href="/public/css/syntax/monokai.css" rel="stylesheet">
+
+    <script>
+    var _hmt = _hmt || [];
+    (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?01953a8cd2292db701fd4960a9deeabd";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+    })();
+    </script>
+
+
 </head>
