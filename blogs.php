342d2915e204438570916e4b922dd1f45eba6bd1
Coder.AN <an.hongjun@foxmail.com>
2024-01-31 14:51:26 +0800

    add redirect

--- blogs.php
+++ blogs.php
@@ -9,6 +9,8 @@
         $__blog = get_markdown($_GET["url"], true);
         if (isset($__blog["external"][0])) {
             $__content = file_get_contents(urldecode("https://".trim($__blog["external"][0])));
+            if (isset($__blog["redirect"][0]))
+                $__content = str_replace("./", "https://".trim($__blog["redirect"][0]), $__content);
             $__content = $Parsedown->text($__content);
         }
         else {
